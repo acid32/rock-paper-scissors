142dc61f697f4a42a24cf7a2f8be1d0087baeb85
kap64 <60051988+kap64@users.noreply.github.com>
2021-02-16 17:36:14 +0100

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -18,38 +18,38 @@ def lose():
 
 while True:
     cpu = random.choice(hands)
-    user = input("\n> ")
+    user = input("\n> ").lower().strip()
 
     if user == "exit" or user == "'exit'":
         break
 
     else:
 
-        if user.lower().strip() not in hands:
+        if user not in hands:
             print("ERROR")
 
-        if user.lower().strip() in hands:
+        if user in hands:
             print("< " + cpu)
 
-            if user.lower().strip() == cpu:
+            if user == cpu:
                 print("Tie")
 
-            elif user.lower().strip() == "rock" and cpu == "scissors":
+            elif user == "rock" and cpu == "scissors":
                 win()
     
-            elif user.lower().strip() == "scissors" and cpu == "paper":
+            elif user == "scissors" and cpu == "paper":
                 win()
 
-            elif user.lower().strip() == "paper" and cpu == "rock":
+            elif user == "paper" and cpu == "rock":
                 win()
 
-            elif user.lower().strip() == "rock" and cpu == "paper":
+            elif user == "rock" and cpu == "paper":
                 lose()
 
-            elif user.lower().strip() == "paper" and cpu == "scissors":
+            elif user == "paper" and cpu == "scissors":
                 lose()
 
-            elif user.lower().strip() == "scissors" and cpu == "rock":
+            elif user == "scissors" and cpu == "rock":
                 lose()
 
             print(str(user_score) + "/" + str(cpu_score))
